731193392d5c53518eb2c9f9a57d0e2dd435ff71
AndrewTrinity <andrewtrinity5000@gmail.com>
2021-02-18 18:02:06 +0100

    Added a new excercise and the explanations on all of them

--- Even_Or_Odd.js
+++ Even_Or_Odd.js
@@ -1,3 +1,6 @@
+//Create a function (or write a script in Shell) that takes an integer as an argument 
+//and returns "Even" for even numbers or "Odd" for odd numbers.
+
 function even_or_odd(number) {
     if (number % 2 === 0) {
         return "Even"
@@ -6,3 +9,4 @@ function even_or_odd(number) {
     }
 }
 console.log(even_or_odd(5))
+console.log(even_or_odd(2))
