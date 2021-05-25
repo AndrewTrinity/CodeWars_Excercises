d652dc0a3e21e931901e6aca550858d656db4ac7
AndrewTrinity <andrewtrinity5000@gmail.com>
2021-05-25 16:33:48 +0200

    Added new excersises

---
+++ Can_we_divide_it.js
@@ -0,0 +1,10 @@
+//Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+
+function isDivideBy(number, a, b) {
+    // good luck
+    if (number / a === b) {
+        return true
+    } else {
+        return false
+    }
+}
